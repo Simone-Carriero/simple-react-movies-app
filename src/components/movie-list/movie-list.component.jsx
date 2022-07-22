47d199af2ebb295ca9600a3b2b169f456809a8b6
Simone Carriero <simonecarriero95@hotmail.com>
2022-07-22 04:21:49 +0200

    Fixed movie cards and some things of style

--- src/components/movie-list/movie-list.component.jsx
+++ src/components/movie-list/movie-list.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SwiperContainer from '../swiper.component';
 import { SwiperSlide } from 'swiper/react';
 import "./movie-list.styles.css"
-import Spinner from '../spinner/spinner.component';
+
 
 const MovieList = ({ movies, handleFavorite, icon }) => {
 
@@ -10,22 +10,14 @@ const MovieList = ({ movies, handleFavorite, icon }) => {
 
     const mappedMovies = movies.filter(movie => movie.poster_path).map(movie => (
         <SwiperSlide className="image-container" key={movie.id} onClick={() => handleFavorite(movie)}>
-            {
-                !movie.poster_path ?
-                    <Spinner />
-                    :
-                    <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={`${movie.original_title} poster`} />
+            
+            <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={`${movie.original_title} poster`} />
 
-            }
+            
             <div className="overlay-container">
-                <div className="icon-container">
-                    <IconComponent />
-                </div>
-                <div className="overlay">
-                    <div className="overlay-container-inside">
-                        <IconComponent />
-                    </div>
-                </div>
+
+                <IconComponent />
+             
             </div>
         </SwiperSlide>
     ))
